64d8e51f129f07ddd98caa86afce93a497bc7131
Maksym Starodubtsev <maksym.starodubtsev@gmail.com>
2021-09-08 22:36:07 +0300

    build problems

---
+++ src/components/RequestsFromStudent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex flex-row justify-between w-full">
+    <CardTemplate v-for="message in requestsFromStudent" :key="message.coachName">
+      <div class="text-left"><span class="font-bold text-left">from</span> - {{message.email}}</div>
+      <div class="text-left"><span class="font-bold text-left">to</span> - {{message.coachName}}</div>
+      <div class="text-left"><span class="font-bold text-left">message:</span> {{message.desc}} </div>
+    </CardTemplate>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import CardTemplate from './CardsCoachList/CardTemplate.vue'
+
+export default ({
+  data () {
+    return {
+      tableData: this.requestsFromStudent
+    }
+  },
+  components: {
+    CardTemplate
+  },
+  computed: {
+    ...mapGetters(['requestsFromStudent'])
+  },
+  methods: {
+    ...mapActions(['getRequests'])
+  },
+  beforeMount () {
+    this.$store.dispatch('getRequests')
+  }
+})
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: false,
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {}
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
