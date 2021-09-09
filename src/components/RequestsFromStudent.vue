d7300ab39b426e888d24c0c2ff65bcfce323314a
Maksym Starodubtsev <maksym.starodubtsev@gmail.com>
2021-09-09 11:07:45 +0300

    add flex fix to request mes,fixed 404 - test 1

--- src/components/RequestsFromStudent.vue
+++ src/components/RequestsFromStudent.vue
@@ -1,25 +1,21 @@
 <template>
-  <div class="flex flex-row justify-between w-full">
-    <CardTemplate v-for="message in requestsFromStudent" :key="message.coachName">
+  <div class="flex flex-wrap w-full">
+    <div v-for="message in requestsFromStudent" :key="message.coachName" class="Border-header">
       <div class="text-left"><span class="font-bold text-left">from</span> - {{message.email}}</div>
       <div class="text-left"><span class="font-bold text-left">to</span> - {{message.coachName}}</div>
       <div class="text-left"><span class="font-bold text-left">message:</span> {{message.desc}} </div>
-    </CardTemplate>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
-import CardTemplate from './CardsCoachList/CardTemplate.vue'
 
 export default ({
   data () {
     return {
       tableData: this.requestsFromStudent
     }
-  },
-  components: {
-    CardTemplate
   },
   computed: {
     ...mapGetters(['requestsFromStudent'])
@@ -32,3 +28,12 @@ export default ({
   }
 })
 </script>
+
+<style scoped>
+  .Border-header {
+    margin: 1rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 1rem;
+    border-radius: 25px;
+  }
+</style>
